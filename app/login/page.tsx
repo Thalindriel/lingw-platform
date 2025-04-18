11d4938ab484cbfb458aba2d49825040f80e964f
Thalindriel <dimasikataz@gmail.com>
2025-04-18 12:11:03 +0500

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,6 +28,10 @@ useEffect(() => {
     )
   }
 
+  if (session) {
+  return null;
+}
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
