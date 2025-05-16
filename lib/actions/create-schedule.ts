383158d0abd7c4a1f3be9932fea1f00aa55a91bc
Thalindriel <dimasikataz@gmail.com>
2025-05-17 02:32:14 +0500

    Update create-schedule.ts

--- lib/actions/create-schedule.ts
+++ lib/actions/create-schedule.ts
@@ -9,9 +9,8 @@ export async function createScheduleForUser(userId: string, courseId: string) {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     { cookies }
-    console.log("Проверка scheduleEntries:", scheduleEntries);
   );
-
+console.log("Проверка scheduleEntries:", scheduleEntries);
   const { data: lessons, error } = await supabase
     .from("lessons")
     .select("id, title")
